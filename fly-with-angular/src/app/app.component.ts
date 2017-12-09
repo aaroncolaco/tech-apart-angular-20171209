feb204989274a43bb8e0e9b6f4632428e0906c82
Aaron Colaço <aaroncolaco.work@gmail.com>
2017-12-09 13:41:11 +0530

    Add basic trips list

--- fly-with-angular/src/app/app.component.ts
+++ fly-with-angular/src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  title: string = 'app';
+  name: string;
+  cost: number;
+  description: string;
+  duration: string;
+  imageUrl: string;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.name = 'Super flight to Venus';
+    this.cost = 18200;
+    this.duration = '2 Earth Years';
+    this.description = 'Race through the Solar System to reach the Red planet. This flight will take 11 Earth Years for each side of the journey.';
+    this.imageUrl = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/Planets2013.svg/2000px-Planets2013.svg.png'
+  }
 }
+
