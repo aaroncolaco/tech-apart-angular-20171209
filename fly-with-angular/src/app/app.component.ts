d4e1248871e153790db79b94fba34a5c6a3000c7
Aaron Colaço <aaroncolaco.work@gmail.com>
2017-12-09 13:58:27 +0530

    Add interface

--- fly-with-angular/src/app/app.component.ts
+++ fly-with-angular/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Trip } from './models/trip';
 
 @Component({
   selector: 'app-root',
@@ -6,21 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title: string = 'app';
-  name: string;
-  cost: number;
-  description: string;
-  duration: string;
-  imageUrl: string;
+  trip: Trip;
 
   constructor() { }
 
   ngOnInit() {
-    this.name = 'Super flight to Venus';
-    this.cost = 18200;
-    this.duration = '2 Earth Years';
-    this.description = 'Race through the Solar System to reach the Red planet. This flight will take 11 Earth Years for each side of the journey.';
-    this.imageUrl = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/Planets2013.svg/2000px-Planets2013.svg.png'
+    this.trip = {
+      name: 'Super flight to Venus',
+      cost: 18200,
+      duration: '2 Earth Years',
+      description: 'Race through the Solar System to reach the Red planet. This flight will take 11 Earth Years for each side of the journey.',
+      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/Planets2013.svg/2000px-Planets2013.svg.png'
+    };
   }
 }
 
---
+++ fly-with-angular/src/app/models/trip.ts
@@ -0,0 +1,7 @@
+export interface Trip {
+  name: string;
+  cost: number;
+  description: string;
+  duration: string;
+  imageUrl: string;
+};
