d6eb89c903a6a76e37dac64ef21136b6e31c21ea
Aaron Colaço <aaroncolaco.work@gmail.com>
2017-12-09 18:26:37 +0530

    Final App

--- fly-with-angular/src/app/components/trips-list/trips-list.component.ts
+++ fly-with-angular/src/app/components/trips-list/trips-list.component.ts
@@ -12,6 +12,8 @@ export class TripsListComponent implements OnInit {
   title = 'Flights of a Lifetime';
   addTripFormMode = false;
   tripForm: FormGroup;
+  amountForm: FormGroup;
+  totalMoney: number;
 
   trips: Array<Trip> = [
     {
@@ -19,37 +21,45 @@ export class TripsListComponent implements OnInit {
       price: 18200,
       duration: '2 Earth Years',
       description: 'Race through the Solar System to reach the Red planet. This flight will take 11 Earth Years for each side of the journey.',
-      image_url: 'https://angular.io/assets/images/logos/angular/angular.png'
+      image_url: 'https://angular.io/assets/images/logos/angular/angular.png',
+      booked: false
     },
     {
       name: 'Super Flights to Mars',
       price: 18200,
       duration: '2 Earth Years',
       description: 'Race through the Solar System to reach the Red planet. This flight will take 11 Earth Years for each side of the journey.',
-      image_url: 'https://angular.io/assets/images/logos/angular/angular.png'
+      image_url: 'https://angular.io/assets/images/logos/angular/angular.png',
+      booked: false
     },
     {
       name: 'Super Flights to Mars',
       price: 18200,
       duration: '2 Earth Years',
       description: 'Race through the Solar System to reach the Red planet. This flight will take 11 Earth Years for each side of the journey.',
-      image_url: 'https://angular.io/assets/images/logos/angular/angular.png'
+      image_url: 'https://angular.io/assets/images/logos/angular/angular.png',
+      booked: false
     }
   ];
 
   constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
-    this.createForm();
+    this.createForms();
+    this.totalMoney = 0;
   }
 
-  createForm() {
+  createForms() {
     this.tripForm = this.fb.group({
       name: new FormControl('', Validators.required),
       price: new FormControl(0, Validators.required),
       duration: new FormControl('', Validators.required),
       description: new FormControl('', Validators.required),
       imageUrl: new FormControl('', Validators.required)
+    });
+
+    this.amountForm = this.fb.group({
+      totalMoney: new FormControl()
     });
   }
 
@@ -58,16 +68,36 @@ export class TripsListComponent implements OnInit {
   }
 
   formSubmitted() {
-    const newTrip = {
+    const newTrip: Trip = {
       name: this.tripForm.controls.name.value,
       price: this.tripForm.controls.price.value,
       duration: this.tripForm.controls.duration.value,
       description: this.tripForm.controls.description.value,
-      image_url: this.tripForm.controls.imageUrl.value
+      image_url: this.tripForm.controls.imageUrl.value,
+      booked: false
     };
 
     this.trips.push(newTrip);
     this.toggleDisplayMode();
   }
+
+  updateTotalMoney() {
+    this.totalMoney = this.amountForm.controls.totalMoney.value;
+  }
+
+  bookTrip(index: number) {
+    const trip = this.trips[index];
+
+    const confirmBooking = () => {
+      this.totalMoney -= trip.price;
+      trip.booked = true;
+    };
+    const insufficientMoneyAlert = () => alert('Insufficient amount of money.');
+
+    (this.totalMoney - trip.price) < 0 ? insufficientMoneyAlert() : confirmBooking();
+
+
+  }
+
 
 }
