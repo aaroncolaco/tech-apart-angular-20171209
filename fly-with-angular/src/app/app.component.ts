c70ada8764b9fc872fa1e83b622738b0d8f15a7c
Aaron Colaço <aaroncolaco.work@gmail.com>
2017-12-09 14:05:51 +0530

    Add array and iterate

--- fly-with-angular/src/app/app.component.ts
+++ fly-with-angular/src/app/app.component.ts
@@ -7,18 +7,26 @@ import { Trip } from './models/trip';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  trip: Trip;
-
+  trips: Trip[];
   constructor() { }
 
   ngOnInit() {
-    this.trip = {
-      name: 'Super flight to Venus',
-      cost: 18200,
-      duration: '2 Earth Years',
-      description: 'Race through the Solar System to reach the Red planet. This flight will take 11 Earth Years for each side of the journey.',
-      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/Planets2013.svg/2000px-Planets2013.svg.png'
-    };
+    this.trips = [
+      {
+        name: 'Super flight to Venus',
+        cost: 18200,
+        duration: '2 Earth Years',
+        description: 'Race through the Solar System to reach the Red planet. This flight will take 11 Earth Years for each side of the journey.',
+        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/Planets2013.svg/2000px-Planets2013.svg.png'
+      },
+      {
+        name: 'Escape to Moon',
+        cost: 900,
+        duration: '1 Earth Month',
+        description: 'Perfect for a short holiday',
+        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/Planets2013.svg/2000px-Planets2013.svg.png'
+      }
+    ];
   }
 }
 
